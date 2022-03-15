34ab3eca144c68208891f129bb2fb92e0fc145f5
oittaa <8972248+oittaa@users.noreply.github.com>
2022-03-15 22:37:31 +0100

    refactor and test randomInt distribution (#14)

--- random.js
+++ random.js
@@ -5,30 +5,14 @@ const DEFAULT_ENTROPY = 32
 const RAND_MAX = 0xFFFF_FFFF_FFFF
 
 /**
-* Return the number of bits necessary to represent an unsigned integer in binary.
-*/
-function bitLength (n) {
-  let x = 0
-  while (n > 0) {
-    x++
-    n = intDiv(n, 2)
-  }
-  return x
-}
-
-/**
  * Return the integer quotient of the division of a by b.
  */
-function intDiv (a, b) {
-  return (a - a % b) / b
-}
+const intDiv = (a, b) => (a - a % b) / b
 
 /**
  * Return a randomly-chosen element from a non-empty array.
  */
-function choice (arr) {
-  return arr[randomInt(arr.length)]
-}
+const choice = arr => arr[randomInt(arr.length)]
 
 /**
  * Return an int with k random bits.
@@ -55,9 +39,7 @@ function randomBits (k) {
  * Generates cryptographically strong pseudorandom data. The size argument
  * is a number indicating the number of bytes to generate.
  */
-function randomBytes (size) {
-  return window.crypto.getRandomValues(new Uint8Array(size))
-}
+const randomBytes = size => window.crypto.getRandomValues(new Uint8Array(size))
 
 /**
 * Return a random integer n such that min <= n < max.
@@ -83,7 +65,11 @@ function randomInt (min, max) {
   if (range === 1) {
     return min
   }
-  const k = bitLength(range)
+  // number of bits necessary to represent an unsigned integer (range) in binary
+  let k = 0
+  for (let i = 1; i <= range; i *= 2) {
+    k++
+  }
   let x
   do {
     x = randomBits(k)
@@ -96,10 +82,7 @@ function randomInt (min, max) {
  * bytes, each byte converted to two hex digits. If numBytes is not supplied,
  * a reasonable default is used.
  */
-function tokenHex (numBytes) {
-  if (typeof numBytes === 'undefined') {
-    numBytes = DEFAULT_ENTROPY
-  }
+function tokenHex (numBytes = DEFAULT_ENTROPY) {
   const byteArray = randomBytes(numBytes)
   return Array.from(byteArray, function (byte) {
     return ('0' + byte.toString(16)).slice(-2)
