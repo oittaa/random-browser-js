96c342d36abb238f4b7cc29e17a51e68e857f8aa
oittaa <8972248+oittaa@users.noreply.github.com>
2022-03-17 10:22:30 +0100

    randomBits optimization (#20)

--- random.test.js
+++ random.test.js
@@ -82,63 +82,96 @@ test.each(arr100)('tokenHex() length', (i) => {
   expect(value.length).toBe(i * 2)
 })
 
-test('choice([])', () => {
-  expect(() => r.choice([])).toThrow(RangeError)
-})
+describe('errors', () => {
+  test('choice([])', () => {
+    expect(() => r.choice([])).toThrow(RangeError)
+  })
 
-test('randomBits(-1)', () => {
-  expect(() => r.randomBits(-1)).toThrow(RangeError)
-})
+  test('randomBits(-1)', () => {
+    expect(() => r.randomBits(-1)).toThrow(RangeError)
+  })
 
-test('randomBits(49)', () => {
-  expect(() => r.randomBits(49)).toThrow(RangeError)
-})
+  test('randomBits(49)', () => {
+    expect(() => r.randomBits(49)).toThrow(RangeError)
+  })
 
-test('randomBytes(-1)', () => {
-  expect(() => r.randomBytes(-1)).toThrow(RangeError)
-})
+  test('randomBytes(-1)', () => {
+    expect(() => r.randomBytes(-1)).toThrow(RangeError)
+  })
 
-test('randomInt(-1, 0xFFFF_FFFF_FFFF)', () => {
-  expect(() => r.randomInt(-1, 0xFFFF_FFFF_FFFF)).toThrow(RangeError)
-})
+  test('randomInt(-1, 0xFFFF_FFFF_FFFF)', () => {
+    expect(() => r.randomInt(-1, 0xFFFF_FFFF_FFFF)).toThrow(RangeError)
+  })
 
-test('randomInt() without safe integer', () => {
-  expect(() => r.randomInt(Number.MAX_SAFE_INTEGER + 1)).toThrow(Error)
-  expect(() => r.randomInt(Number.MIN_SAFE_INTEGER - 1, Number.MIN_SAFE_INTEGER)).toThrow(Error)
-})
-
-// Doesn't guarantee correctness, but at least the numbers are appearing in the full range.
-test.each([2, 14, 15, 16, 17, 254, 255, 256, 257])('randomInt() distribution ', (m) => {
-  const dict = {}
-  for (let i = 0; i < 100_000; i++) {
-    const n = r.randomInt(m)
-    if (n in dict) {
-      dict[n]++
-    } else {
-      dict[n] = 1
-    }
-  }
-  expect(Object.keys(dict).length).toBe(m)
-  const values = Object.values(dict)
-  const max = Math.max(...values)
-  const min = Math.min(...values)
-  expect(min / max).toBeGreaterThan(0.5)
+  test('randomInt() without safe integer', () => {
+    expect(() => r.randomInt(Number.MAX_SAFE_INTEGER + 1)).toThrow(Error)
+    expect(() => r.randomInt(Number.MIN_SAFE_INTEGER - 1, Number.MIN_SAFE_INTEGER)).toThrow(Error)
+  })
 })
 
 // Doesn't guarantee correctness, but at least the numbers are appearing in the full range.
-test.each([1, 2, 3, 4, 5, 6, 7, 8, 9])('randomBits() distribution ', (m) => {
-  const dict = {}
-  for (let i = 0; i < 100_000; i++) {
-    const n = r.randomBits(m)
-    if (n in dict) {
-      dict[n]++
-    } else {
-      dict[n] = 1
+describe('distribution', () => {
+  test.each([1, 8, 65536])('randomBytes()', (m) => {
+    const dict = {}
+    for (let i = 0; i < 1_000_000 / m; i++) {
+      const bytes = r.randomBytes(m)
+      for (let j = 0; j < bytes.length; j++) {
+        if (bytes[j] in dict) {
+          dict[bytes[j]]++
+        } else {
+          dict[bytes[j]] = 1
+        }
+      }
     }
-  }
-  expect(Object.keys(dict).length).toBe(Math.pow(2, m))
-  const values = Object.values(dict)
-  const max = Math.max(...values)
-  const min = Math.min(...values)
-  expect(min / max).toBeGreaterThan(0.5)
+    expect(Object.keys(dict).length).toBe(256)
+    const values = Object.values(dict)
+    const max = Math.max(...values)
+    const min = Math.min(...values)
+    expect(min / max).toBeGreaterThan(0.8)
+  })
+
+  test.each([2, 14, 15, 16, 17, 254, 255, 256, 257])('randomInt()', (m) => {
+    const dict = {}
+    for (let i = 0; i < 100_000; i++) {
+      const n = r.randomInt(m)
+      if (n in dict) {
+        dict[n]++
+      } else {
+        dict[n] = 1
+      }
+    }
+    expect(Object.keys(dict).length).toBe(m)
+    const values = Object.values(dict)
+    const max = Math.max(...values)
+    const min = Math.min(...values)
+    expect(min / max).toBeGreaterThan(0.5)
+  })
+
+  test.each([1, 2, 3, 4, 5, 6, 7, 8, 9])('randomBits()', (m) => {
+    const dict = {}
+    for (let i = 0; i < 100_000; i++) {
+      const n = r.randomBits(m)
+      if (n in dict) {
+        dict[n]++
+      } else {
+        dict[n] = 1
+      }
+    }
+    expect(Object.keys(dict).length).toBe(Math.pow(2, m))
+    const values = Object.values(dict)
+    const max = Math.max(...values)
+    const min = Math.min(...values)
+    expect(min / max).toBeGreaterThan(0.5)
+  })
+
+  test('randomBits(48) average', () => {
+    const expected = (2 ** 48 - 1) / 2
+    const rounds = 100_000
+    let avg = 0
+    for (let i = 0; i < rounds; i++) {
+      avg += r.randomBits(48)
+    }
+    avg /= rounds
+    expect(avg / expected).toBeCloseTo(1.0)
+  })
 })
